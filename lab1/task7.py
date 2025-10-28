82a3a5a91fc1de6dce37cae14831365b815397fd
DariaSeniutych <dariaseniutych@yandex.by>
2025-10-28 12:09:22 +0300

    через f строки

--- lab1/task7.py
+++ lab1/task7.py
@@ -1,7 +1,7 @@
-seconds = int(input("введи количество секунд: "))
-minutes = seconds // 60
-ostatok = seconds % 60
-print(minutes, "минут", ostatok, "секунд")
+# seconds = int(input("введи количество секунд: "))
+# minutes = seconds // 60
+# ostatok = seconds % 60
+# print(minutes, "минут", ostatok, "секунд")
 
 
 
@@ -9,3 +9,4 @@ seconds = int(input("введи количество секунд: "))
 minutes = seconds // 60
 ostatok = seconds % 60
 print(f"{minutes} минут {ostatok} секунд")
+
